94498485b06e9740493a5e545134fa0bd973d1f9
ziaasifmuhammad <155935485+ziaasifmuhammad@users.noreply.github.com>
2024-11-01 10:12:30 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,6 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <div>
-<title>CAR CROWD</title>
-<meta name="description" content="It is all about used and new car"/>
 
 <div className="h-[80vh]">
 <Hero/>
